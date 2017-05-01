2abfa4cb4b22da3eb06ce9d96232d70fb03dca7c
Napon Mekavuthikul <napon.meka@gmail.com>
2017-05-01 12:17:57 +0700

    data + initial code

---
+++ dtw.py
@@ -0,0 +1,49 @@
+from math import *
+import sys
+import time
+start = time.time()
+train_filename = 'Beef_TRAIN'
+test_filename = 'Beef_TEST'
+f = open('ClassificationClusteringDatasets/' + train_filename)
+data_train = []
+for line in f:
+    floats = [float(x) for x in line.strip().split(' ')]
+    data_train.append(floats)
+f.close()
+
+data_test = []
+f = open('ClassificationClusteringDatasets/' + test_filename)
+for line in f:
+    floats = [float(x) for x in line.strip().split(' ')]
+    data_test.append(floats)
+f.close()
+
+def DTWDistance(s1, s2, w):
+    DTW={}
+    w = max(w, abs(len(s1)-len(s2)))
+    for i in range(-1, len(s1)):
+        for j in range(-1, len(s2)):
+            DTW[(i, j)] = float('inf')
+    DTW[(-1, -1)] = 0
+    for i in range(len(s1)):
+        for j in range(max(0, i-w), min(len(s2), i+w)):
+            dist= (s1[i]-s2[j])**2
+            # DTW[(i, j)] = dist + min(DTW[(i-1, j)],DTW[(i, j-1)], DTW[(i-1, j-1)])
+            DTW[(i, j)] = min(DTW[(i-1, j)] + dist ,DTW[(i, j-1)] + dist, DTW[(i-1, j-1)] + 2*dist)
+    return sqrt(DTW[len(s1)-1, len(s2)-1])
+
+correct_predictions = 0
+
+for idx,d_test in enumerate(data_test):
+    min_dist = inf
+    the_label = 0
+    for d_train in data_train:
+        distance = DTWDistance(d_test[1:],d_train[1:],10)
+        if distance < min_dist:
+            min_dist = distance
+            the_label = d_train[0]
+    if the_label == d_test[0]:
+        correct_predictions += 1
+    end = time.time()
+    print("index:{}, time:{}".format(idx, end - start))
+print(correct_predictions/len(data_test))
