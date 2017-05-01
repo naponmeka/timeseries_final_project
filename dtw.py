68af89a348183f77c2b01516d8bee276300dfb81
Keerato <Keerati.tan@gmail.com>
2017-05-01 13:59:59 +0700

    upper_bound / lower_bound trim space

--- dtw.py
+++ dtw.py
@@ -1,14 +1,49 @@
 from math import *
+from scipy.otimize import basinhopping
 import sys
 import time
+
+def upper_keogh(seq):
+    upper_seq = []
+    for i in range(len(seq)):
+        max_value = seq[i]
+        for j in range(r):
+            if i - j < 0: continue
+            if seq[i-j] > max_value: max_value = seq[i-j]
+        for j in range(r):
+            if i + j >= len(seq): continue
+            if seq[i+j] > max_value: max_value = seq[i-j]
+        upper_seq.append(max_value)
+    return upper_seq
+
+def lower_keogh(seq):
+    lower_seq = []
+    for i in range(len(seq)):
+        min_value = seq[i]
+        for j in range(r):
+            if i - j < 0: continue
+            if seq[i-j] < min_value: min_value = seq[i-j]
+        for j in range(r):
+            if i + j >= len(seq): continue
+            if seq[i+j] < min_value: min_value = seq[i-j]
+        lower_seq.append(min_value)
+    return lower_seq
+
 start = time.time()
 train_filename = 'Beef_TRAIN'
 test_filename = 'Beef_TEST'
 f = open('ClassificationClusteringDatasets/' + train_filename)
 data_train = []
+train_lower_b = []
+train_upper_b = []
+window_size = 10
+r = 5
+
 for line in f:
     floats = [float(x) for x in line.strip().split(' ')]
     data_train.append(floats)
+    train_upper_b.append(upper_keogh(floats[1:]))
+    train_lower_b.append(lower_keogh(floats[1:]))
 f.close()
 
 data_test = []
@@ -39,17 +74,29 @@ def DTWDistance(s1, s2, w):
 
 correct_predictions = 0
 
+def LB_Keogh(C, idx):
+    sum_weight = 0
+    for i in range(len(C)):
+        if C[i] > train_upper_b[idx][i]:
+            sum_weight += (C[i] - train_upper_b[idx][i]) ** 2
+        elif C[i] < train_lower_b[idx][i]:
+            sum_weight += (C[i] - train_lower_b[idx][i]) ** 2
+    return sum_weight
+        
 for idx,d_test in enumerate(data_test):
     min_dist = float('inf')
     the_label = 0
-    for d_train in data_train:
-        #LB_dist = LB_Keogh()
-        distance = DTWDistance(d_test[1:],d_train[1:],10)
-        if distance < min_dist:
-            min_dist = distance
-            the_label = d_train[0]
+    for idx2, d_train in enumerate(data_train):
+        LB_dist = LB_Keogh(d_test[1:], idx2)
+        if LB_dist < min_dist:
+            distance = DTWDistance(d_test[1:],d_train[1:],window_size)
+            if distance < min_dist:
+                min_dist = distance
+                the_label = d_train[0]
     if the_label == d_test[0]:
         correct_predictions += 1
     end = time.time()
     print("index:{}, time:{}".format(idx, end - start))
 print(correct_predictions/len(data_test))
+
+func = lambda seq: LB_Keogh(seq,0)
