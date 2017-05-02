dadba1cc8d71890287209aff70428476832caf19
Kinnch <kinnch@gmail.com>
2017-05-02 16:28:04 +0700

    remove duplicated code

--- dtw-noob.py
+++ dtw-noob.py
@@ -57,35 +57,6 @@ f.close()
 # weight1 = 1
 # weight2 = 1
 # weight3 = 1
-def DTWDistance(s1, s2, w, weight1, weight2, weight3):
-    DTW={}
-    w = max(w, abs(len(s1)-len(s2)))
-    for i in range(-2, len(s1)):
-        for j in range(-2, len(s2)):
-            DTW[(i, j)] = float('inf')
-    DTW[(-1, -1)] = 0
-    for i in range(len(s1)):
-        for j in range(max(0, i-w), min(len(s2), i+w)):
-            dist = (s1[i]-s2[j])**2
-            # DTW[(i, j)] = dist + min(DTW[(i-1, j)],DTW[(i, j-1)], DTW[(i-1, j-1)])
-            DTW[(i, j)] = min(DTW[(i-1, j-1)] + weight1 * dist,
-                            DTW[(i-1, j)] + weight2 * dist,
-                            DTW[(i, j-1)] + weight3 * dist)
-    return sqrt(DTW[len(s1)-1, len(s2)-1])
-
-def DTWCostMatrix(s1, s2, w):
-    DTW={}
-    w = max(w, abs(len(s1)-len(s2)))
-    for i in range(-2, len(s1)):
-        for j in range(-2, len(s2)):
-            DTW[(i, j)] = float('inf')
-    DTW[(-1, -1)] = 0
-    for i in range(len(s1)):
-        for j in range(max(0, i-w), min(len(s2), i+w)):
-            dist = (s1[i]-s2[j])**2
-            # DTW[(i, j)] = dist + min(DTW[(i-1, j)],DTW[(i, j-1)], DTW[(i-1, j-1)])
-    # return sqrt(DTW[len(s1)-1, len(s2)-1])
-    return DTW
 
 correct_predictions = 0
 
